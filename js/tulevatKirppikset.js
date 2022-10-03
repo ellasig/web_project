3e721dba4c4c423dc0e759bfe800fef34a6fb21b
tuiskuna <tuiskunarhi@gmail.com>
2022-10-03 14:51:01 +0300

    lisätty uusi html ja js

--- js/tulevatKirppikset.js
+++ js/tulevatKirppikset.js
@@ -1,2 +1,19 @@
+/*
+* Kehittäjät: Tuisku Närhi, Ella Sigvart, Sanna Lohkovuori
+* Versio 1.1
+* 1.10.2022
+* */
 
 'use strict'
+
+//Käytetään APIa tälle sivulle.
+
+const apiurl = "https://open-api.myhelsinki.fi/v1/events/?tags_search=flea%20markets";
+
+// lopullinen hakukysely, joka lähetetään nettiin.
+let apiKysely;
+
+// Etsitään HTML-sivulta tarvittavat komponentit id:n avulla.
+const hakunappi = document.getElementById("hakunappi");
+const hakukentta = document.getElementById('haku');
+const mainElem = document.querySelector('main');
