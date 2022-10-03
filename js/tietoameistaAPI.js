2878d92f14c485954ce1699bdbfb77a177b6c684
ellasig <ellla.si@hotmail.com>
2022-10-03 14:09:46 +0300

    navigointisivujen tekoa

---
+++ js/tietoameistaAPI.js
@@ -0,0 +1,12 @@
+/*
+* Kehittäjät: Tuisku Närhi, Ella Sigvart, Sanna Lohkovuori
+* Versio 1.1
+* 1.10.2022
+* */
+
+'use strict';
+
+//Api'n käyttö:
+
+// hakuosoitteen vakio-osa.
+//const apiurl = "https://api.tvmaze.com/search/shows?q="; //hakuavain eritetään ?
