42581ba78382590878c1d1edeeb3f00c01b0358b
tuiskuna <tuiskunarhi@gmail.com>
2022-10-05 09:34:03 +0300

    eri kirppiksia kuvat säädetty saman kokoisiksi ja flexboxia korjailtu hiukan

--- js/erilaisiakirpputoreja.js
+++ js/erilaisiakirpputoreja.js
@@ -19,31 +19,31 @@ function navbarClass() {
 const picArray = [
     {
         title: 'Fida',
-        caption: '',
+        alt: 'kuva fidan logosta',
         description: 'Fida on kotimainen hyväntekeväisyysmyymäläketju. Löydät heiltä vaatteita, huonekaluja ja harrastustarvikkeita. Otamme vastaan hyvänkuntoisia vaatteita, huonekaluja ja muuta kodin irtaimistoa. ',
         filename: 'kuvat/fida_logo.jpg'
     },
     {
         title: 'UFF',
-        caption: '',
+        alt: 'kuva uffin logosta',
         description: 'UFF on yleishyödyllinen säätiö, joka ylläpitää vaatekeräyspalvelua Suomessa sekä tukee globaalikehitystyötä Afrikassa ja Aaasiassa. UFF:lta löytyy vaatteille lahjoituslaatikoita ympäri kaupunkeja sekä heidän toimipisteelle voi toimittaa ehjiä ja puhtaita vaatteita sekä tekstiilejä.',
         filename: 'kuvat/uff.jpg'
     },
     {
         title: 'Relove',
-        caption: '',
+        alt: 'kuva reloven logosta',
         description: 'Relovessa laadukkaita second hand-aarteita sekä kahvilassa tarjoillaan aamiaista, lounasta ja käsintehtyjö leipomuksia viikon jokaisena päivänä',
         filename: 'kuvat/relove.jpg'
     },
     {
         title: 'Kierrätyskeskus',
-        caption: '' ,
+        alt: 'kuva kierrätyskeskuksen logosta' ,
         description: 'Kierrätyskeskuksella on pääkaupungiseudulla 12 kierrätysmyymälää sekä valtakunnallisesti toimiva verkkokauppa. Heille voi viedä ehjät ja tarpeettomat esineet, huonekalut sekä vaatteet. Kierrätyskeskusella on myös noutopalvelu isomille huonekaluille.',
         filename: 'kuvat/kierratyskeskus1.jpeg'
     },
     {
         title:'HELSINKI FLEA MARKET',
-        caption: '',
+        alt: 'kuva helsinki flea marketin logosta',
         description: 'Helsinki flea market on uusi itsepalvelukirpputori joka sijaitsee Töölössä, Helsingin virkistysalueen sydämessä. Kaikki varattavissa olevat pöydät ovat rakennettu kierrätysmateriaaleista esimerkiksi teholaivoista ja melamiinilevyistä. Heiltä löytyy myös mobiilisovellus mistä voi seurata omia myyntejä ja luomaan hintalappuja suoraan kotisohvalta.',
         filename: 'kuvat/fleamarket.jpg'
     }
@@ -54,10 +54,12 @@ const mainElem = document.querySelector('main');
 for (let i = 0; i < picArray.length; i++) {
 
     let articleElem = document.createElement('article');
+    articleElem.className = 'kirppariArticlet';
 
     let headerElem = document.createElement('header');
     let h2Elem = document.createElement('h2');
     h2Elem.innerHTML = picArray[i].title
+
 
     headerElem.appendChild(h2Elem);
 
@@ -67,11 +69,7 @@ for (let i = 0; i < picArray.length; i++) {
 
     imgElem.src = picArray[i].filename;
     figureElem.appendChild(imgElem);
-    imgElem.alt = "";
-
-    let figcaptionElem = document.createElement('figcaption');
-    figcaptionElem.innerHTML = picArray[i].caption;
-    figureElem.appendChild(figcaptionElem);
+    imgElem.alt = picArray[i].alt;
 
     let pElem = document.createElement('p');
     pElem.innerHTML = picArray[i].description;
